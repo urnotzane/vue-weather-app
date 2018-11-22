7fe34a23a179d87ef52045b457317f3791e3ef77
urnotzane <urnotzane@163.com>
2018-11-22 20:00:35 +0800

    起步

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="switch-dot"></div>
+    <div class="switch-body">
+      <div class="weather-header"></div>
+      <div class="weather-body">
+        <div class="weather-brief">
+          <div></div>
+        </div>
+        <div class="copy-writing"></div>
+      </div>
+      <div class="weather-footer"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>1231</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
+

4fb53c298adaad973283554a2b11bcbf02bacf26
urnotzane <urnotzane@163.com>
2018-11-23 09:40:38 +0800

    主页大致数据

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,32 @@
       <div class="weather-header"></div>
       <div class="weather-body">
         <div class="weather-brief">
-          <div></div>
+          <div class="brief-city">
+            深圳
+          </div>
+          <div class="brief-tempreture">
+            <span>22°C</span>
+            <span>晴</span>
+          </div>
+          <div class="brief-detail">
+            <div class="detail-item">
+              <div>巳时</div>
+              <div>日出三竿</div>
+            </div>
+            <div class="detail-item">
+              <div>万里无云</div>
+              <div>15°~24°</div>
+            </div>
+            <div class="detail-item">
+              <div>风力2级</div>
+              <div>风向正西</div>
+            </div>
+          </div>
         </div>
-        <div class="copy-writing"></div>
+        <div class="interesting">
+          <div class="interesting-item">无边落木萧萧下</div>
+          <div class="interesting-item">不尽长江滚滚来</div>
+        </div>
       </div>
       <div class="weather-footer"></div>
     </div>
@@ -20,7 +43,12 @@ export default {
 };
 </script>
 
-<style lang="less" scoped>
-
+<style lang="less">
+html {
+  font-size: 16px;
+  body {
+    margin: 0;
+  }
+}
 </style>
 
